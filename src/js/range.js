51f4c6af32ffe8ae16062396783014edef3cb11f
Maurice van Creij <maurice.vancreij@webqem.com>
2019-02-22 07:54:19 +1100

    Update require support.

--- src/js/range.js
+++ src/js/range.js
@@ -34,6 +34,5 @@ var Range = function (config) {
 };
 
 // return as a require.js module
-if (typeof module !== 'undefined') {
-	exports = module.exports = Range;
-}
+if (typeof define != 'undefined') define(['range'], function () { return Range });
+if (typeof module != 'undefined') module.exports = Range;
